48199a3aa50383ff5b6e7dce490b379a2cfa86d1
Thinh Nguyen <thinh.nguyen@meltwater.com>
2017-03-24 22:00:58 -0700

    original commit

---
+++ MeltwaterKit/classes/MWMagicLoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MWMagicLoginViewController.swift
+//  MeltwaterKit
+//
+//  Created by Thinh Nguyen on 3/24/17.
+//  Copyright © 2017 Meltwater. All rights reserved.
+//
+
+import Foundation
